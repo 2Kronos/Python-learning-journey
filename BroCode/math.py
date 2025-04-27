1c6dc55cb520ef77110e5d5705d1035ca72cea91
2Kronos <rayachrinovic@gmail.com>
2025-04-27 22:39:46 +0200

    new

---
+++ BroCode/math.py
@@ -0,0 +1,50 @@
+# x = 3.14
+# y = 4 
+# z = 5
+
+#result = round(x)
+#result =abs(y)
+#result = pow(4, 3)
+#result = max(x, y, z)
+# result = min(x, y, x)
+
+# print(result)
+
+
+#============================= BUILT IN FUNCTIONS ==========================
+
+import math 
+
+#print(math.pi)
+#print(math.e)
+
+# x = 9.1
+# # result = math.sqrt(x)
+
+# #result = math.ceil(x)
+# result = math.floor(x)
+# print(result)
+
+# ------------------------- Circumference of cirlce 
+
+# radius = float(input("Enter the radius of a circle "))
+
+# circum = 2*math.pi * radius
+
+# print(f"The circumference is {round(circum, 2)}cm ")
+
+# ------------------------------------------Area of a circle 
+
+# radius = float(input("Enter the raidus of a cirlce "))
+
+# area = math.pi*pow(radius, 2)
+
+# print(f"The area of the circle is: {round(area, 2)}cm^2")
+
+# --------------------------------------------Hypotenuse of a right angle triangle
+
+a = float(input("Enter side a"))
+b = float(input("Enter side b"))
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(f"Side c is {c}")yo  
